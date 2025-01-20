74dddabb475c09ba1e0be7e4b35fb1675708cf94
Michael Redig <mredig@gmail.com>
2025-01-20 03:10:28 -0600

    (feat) hdlr

---
+++ Sources/MP4ChonksLib/Chonks/HDLR.swift
@@ -0,0 +1,33 @@
+import DataScanner
+import Foundation
+import SwiftPizzaSnips
+
+public struct HDLR: ChonkProtocol {
+	public struct HandlerType: RawRepresentable, Sendable, Hashable, MagicNumber {
+		public var rawValue: UInt32
+
+		public init(rawValue: UInt32) {
+			self.rawValue = rawValue
+		}
+
+		public static let video = HandlerType(stringValue: String("vide".reversed()))!
+		public static let sound = HandlerType(stringValue: String("soun".reversed()))!
+		public static let hint = HandlerType(stringValue: String("hint".reversed()))!
+	}
+
+	public let handlerType: HandlerType
+	public let name: String
+
+	init(decoding data: Data) throws {
+		var scanner = DataScanner(data: data)
+		scanner.defaultEndianness = .big
+
+		let _: (UInt8, NullFlags) = try scanner.scanVersionAndFlags()
+
+		scanner.currentOffset += 4 // skip pre defined
+		let rawHandler: UInt32 = try scanner.scan()
+		self.handlerType = HandlerType(rawValue: rawHandler)
+		scanner.currentOffset += 12 // skip reserved
+		self.name = try scanner.scanStringUntilNullTerminated()
+	}
+}
--- Sources/MP4ChonksLib/Chonks/MDIA.swift
+++ Sources/MP4ChonksLib/Chonks/MDIA.swift
@@ -5,13 +5,14 @@ import SwiftPizzaSnips
 public struct MDIA: ChonkProtocol {
 	public let mdhd: MDHD
 //	public let minf: MINF
-//	public let hdlr: HDLR
+	public let hdlr: HDLR
 
 	public init(decoding data: Data) throws {
 		var scanner = DataScanner(data)
 		scanner.defaultEndianness = .big
 
 		var mdhd: MDHD?
+		var hdlr: HDLR?
 
 		while scanner.isAtEnd == false {
 			let nextBox = try scanner.scanNextBox()
@@ -19,7 +20,9 @@ public struct MDIA: ChonkProtocol {
 			switch nextBox.magic {
 			case MDHD.magic:
 				mdhd = try MDHD(decoding: nextBox.data)
-			case "minf", "hdlr":
+			case HDLR.magic:
+				hdlr = try HDLR(decoding: nextBox.data)
+			case "minf":
 				break
 			default:
 				try ChonkError.debugThrow(
@@ -30,8 +33,12 @@ public struct MDIA: ChonkProtocol {
 		}
 
 		guard let mdhd else {
-			throw ChonkError.missingValue(type: Self.self, value: "Missing MDHD")
+			throw ChonkError.missingValue(type: Self.self, value: "Missing mdhd")
+		}
+		guard let hdlr else {
+			throw ChonkError.missingValue(type: Self.self, value: "Missing hdlr")
 		}
 		self.mdhd = mdhd
+		self.hdlr = hdlr
 	}
 }
